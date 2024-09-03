5bfe7c8bb3b95f32460b5347c1b95aa57c6e356c
min0liveira <134062578+min0liveira@users.noreply.github.com>
2024-09-03 12:01:10 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,36 @@
+export const perguntas = [
+    {  
+        enunciado: "Pedra Filosofal",
+        alternativas: [
+            {
+                texto: "Inicialmente, os garotos acreditavam que a Pedra Filosofal, que se encontrava protegida em Hogwarts, fosse alvo de Severo Snape.",
+                proxima: 1,
+            },
+            {
+                texto: "Através do decorrer do filme, me surpreendi com a história, mas esperava mais do filme.",
+                afirmacao: [
+                    "Através do decorrer do filme, me surpreendi com a história.",
+                    "Esperava mais do filme."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Câmara Secreta",
+        alternativas: [
+            {
+                texto: "O cofundador Salazar Slytherin deixou uma Câmara Secreta, que pode ser aberta apenas por seu herdeiro. A câmara é aberta, libertando um monstro que tenta matar alunos nascidos trouxas.",
+                proxima: 3,
+            },
+            {
+                texto: "Através do filme, aprendi mais sobre a história de Hogwarts e seus fundadores.",
+                afirmacao: [
+                    "Aprendi mais sobre a história de Hogwarts e seus fundadores.",
+                    "O filme trouxe uma nova perspectiva sobre as tradições da escola."
+                ],
+                proxima: 4,
+            },
+        ]
+    }
+];
