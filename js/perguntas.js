eed416589c607175960915104415b24053027a89
min0liveira <134062578+min0liveira@users.noreply.github.com>
2024-09-03 11:18:21 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -7,29 +7,29 @@ export  const  perguntas  =  [
     
                 
                 
-                ] ,
+                ],
                 próximo : 1 ,
             } ,
             {
-                texto : "pedra filosofal" ,
+                texto : "pedra filosofal"
                 afirmação : [
-                    "atraves do decorrer do filme me supreendi com a história" ,
+                    "atraves do decorrer do filme me supreendi com a história" 
                     "esperava mias do filme."
                 
                   
-                  próximo : 2 ,
-            } ,
+                  próximo : 2 
+            } 
         ]
-    } ,
+    } 
     {
         enunciado : "câmera secreta" : [
             {
                 texto : " o cofundador Salazar Slytherin deixou uma Câmara Secreta, que pode ser aberta apenas por seu herdeiro. A câmara é aberta libertando ummonstro que tenta matar alunos nascidos trouxas."
-próximo : 3 ,
-            } ,
+próximo : 3 
+            } 
             {
                 texto : "através do filme"
 
               
-                ] ,
+                ] 
               
